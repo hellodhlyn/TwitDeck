9f0ec46cf5c17d997c8f233fd703030f91895efc
Do Hoerin <hellodhlyn@gmail.com>
2018-07-07 17:24:43 +0900

    Added menu, welcome pannel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="app">
-    <div class="pannel" v-for="(channelId, index) in channelIds" v-bind:key="channelId">
-      <chat :channelId="channelId" v-on:delete-pannel="deleteChatPannel(index)" v-on:add-pannel="addChatPannel()" />
+    <div class="menu pannel">
+      <div class="menu-item" @click="modal.settings.isActive = true">
+        <i class="fas fa-cogs"></i>
+        <p>패널설정</p>
+      </div>
+    </div>
+
+    <div class="hello pannel" v-if="channelIds.length === 0">
+      <h1>TwitDeck!</h1>
+      <p>왼쪽의 <strong>[패널 설정]</strong>을 눌러 채널을 추가해주세요.</p>
+      <p><a href="https://github.com/HelloDHLyn/TwitDeck">GitHub</a></p>
+    </div>
+
+    <div class="chat pannel" v-for="channelId in channelIds" v-bind:key="channelId">
+      <chat :channelId="channelId" v-on:delete-pannel="deletePannel(channelId)" />
+    </div>
+
+    <!-- 설정 모달 -->
+    <div id="settings-modal" v-bind:class="['modal', modal.settings.isActive ? 'active' : '']">
+      <div class="modal-content">
+        <h2>패널 추가</h2>
+        <input text="text" placeholder="채널 ID" v-model="input.channelToAdd">
+        <button type="button" @click="addPannel()">추가</button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,14 +39,24 @@ export default Vue.extend({
   },
   data() {
     return {
-      channelIds: [null],
+      channelIds: [],
+      modal: {
+        settings: {
+          isActive: false,
+        },
+      },
+      input: {
+        channelToAdd: '',
+      },
     };
   },
   methods: {
-    addChatPannel() {
-      this.channelIds.push(null);
+    addPannel() {
+      this.channelIds.push(this.input.channelToAdd);
+      this.modal.settings.isActive = false;
     },
-    deleteChatPannel(index) {
+    deletePannel(channelId) {
+      const index = this.channelIds.indexOf(channelId);
       this.channelIds.splice(index, 1);
     },
   },
@@ -32,31 +64,143 @@ export default Vue.extend({
 </script>
 
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+html {
+  height: 100%;
+  overflow: hidden;
+}
+
 body {
   font-family: 'Spoqa Han Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
-  background: rgb(25, 23, 28);
+  height: 100%;
   margin: 0;
+  padding: 0;
 }
 
 #app {
-  color: #2c3e50;
-
+  color: rgb(218, 216, 222);
+  background-color: rgb(25, 23, 28);
   height: 100vh;
   min-width: 100%;
-  white-space: nowrap;
-}
+  display: flex;
 
-.pannel {
-  height: calc(100vh - 48px);
-  width: 360px;
-  display: inline-block;
-  margin: 12px 0 12px 12px;
-  padding: 12px;
-  background-color: rgb(14, 12, 19);
-  color: rgb(218, 216, 222);
+  a {
+    color: rgb(177, 157, 216);
+  }
+
+  .pannel {
+    padding: 12px;
+    background-color: rgb(14, 12, 19);
+  }
+
+  .menu.pannel {
+    height: 100%;
+    min-height: 100%;
+    text-align: center;
+    padding: 24px 0;
+    margin: 0;
+    width: 80px;
+
+    .menu-item {
+      i {
+        font-size: 28px;
+      }
+
+      p {
+        margin: 0;
+        font-size: 12px;
+      }
+    }
+
+    .menu-item:hover {
+      cursor: pointer;
+    }
+  }
+
+  .hello.pannel {
+    width: 360px;
+    margin: 12px 0 12px 12px;
+    padding: 16px 12px;
+
+    * {
+      word-break: break-all;
+    }
+
+    h1, h2, h3 {
+      margin: 24px 0 0 0;
+      font-weight: normal;
+    }
+
+    h1 {
+      margin: 0;
+    }
+
+    ul {
+      margin: 8px 0;
+    }
+
+    p, li {
+      font-size: 14px;
+    }
+  }
+
+  .chat.pannel {
+    height: calc(100% - 24px);
+    margin: 12px 0 12px 12px;
+    padding: 12px;
+    width: 360px;
+  }
+
+  .modal {
+    visibility: hidden;
+    position: fixed;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+
+    .modal-content {
+      padding: 12px;
+      background-color: rgb(25, 23, 28);
+      margin: 10vh auto;
+      width: 480px;
+
+      h2 {
+        margin: 0 0 12px 0;
+        font-weight: normal;
+      }
+
+      input {
+        height: 32px;
+        border: 0;
+        padding: 0 12px;
+        font-size: 12px;
+      }
+
+      button {
+        margin: 4px;
+        border: 0;
+        padding: 8px 12px;
+        background: rgb(100, 65, 164);
+        color: rgb(218, 216, 222);
+        font-size: 12px;
+      }
+
+      button:hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .modal.active {
+    visibility: initial;
+  }
 }
 </style>
